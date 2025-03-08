4f0108ffe566db9c4377c4e11ec751f516e1587b
dtus <ledoantu04@gmail.com>
2025-03-08 23:27:56 +0700

    done sidebar

--- tlu-website/src/components/user/AdmissionMain.jsx
+++ tlu-website/src/components/user/AdmissionMain.jsx
@@ -1,28 +1,42 @@
-import React from 'react'
-import NavBar from '../layouts/NavBar'
-import Footer from '../layouts/Footer'
-import HighlightHeader from '../layouts/HighlightHeader';
-import ArticleOther from '../layouts/ArticleOther';
+import React from "react";
+import NavBar from "../layouts/NavBar";
+import Footer from "../layouts/Footer";
+import HighlightHeader from "../layouts/HighlightHeader";
+import ArticleOther from "../layouts/ArticleOther";
+import Sidebar from "../layouts/Sidebar";
 
 function AdmissionMain() {
+  const majors = [
+    "Ngành Công nghệ thông tin",
+    "Ngành Kỹ thuật phần mềm",
+    "Ngành Hệ thống thông tin",
+    "Ngành trí tuệ nhân tạo",
+  ];
   return (
     <div>
-        <NavBar />
+      <NavBar />
 
-        <div>
-            <div className='w-[1300px] mx-auto mb-10 mt-30'>
+      <div>
+        <div className="w-[1300px] mx-auto mb-10 mt-30">
+          <div>
+            <div className="flex">
+              <Sidebar items={majors} />
+              <div className="pr-10">
                 <HighlightHeader title="TUYỂN SINH" />
-                <div className='flex flex-col gap-y-5'>
-                    <ArticleOther />
-                    <ArticleOther />
-                    <ArticleOther />
+                <div className="flex flex-col gap-y-5">
+                  <ArticleOther />
+                  <ArticleOther />
+                  <ArticleOther />
                 </div>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
 
-        <Footer />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default AdmissionMain
+export default AdmissionMain;
